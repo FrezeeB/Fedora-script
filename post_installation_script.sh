41a8c550838e111c4b24e5a5ccacdb411cb4501f
FrezeeB <108023373+FrezeeB@users.noreply.github.com>
2024-01-18 23:23:09 -0600

    Update post_installation_script.sh

--- post_installation_script.sh
+++ post_installation_script.sh
@@ -8,13 +8,15 @@ if [ ! -d "/etc/post_installation_script" ]; then # Check if the directory exist
 fi
 destination_directory="/etc/post_installation_script"
 flag_file="$destination_directory"/resume_script_after_reboot
+username=$(who | head -n 1 | awk '{print $1}')
 wget --timeout=60 --continue -O "$destination_directory" https://raw.githubusercontent.com/FrezeeB/Fedora-script/20d3108910476ac67d51d1004ce21e23b86e4a2e/post_installation_script.sh
 
 # Specify URLs for packages
 zoom_url="https://zoom.us/client/latest/zoom_x86_64.rpm"
 libreoffice_url="https://download.documentfoundation.org/libreoffice/stable/7.6.4/rpm/x86_64/LibreOffice_7.6.4_Linux_x86-64_rpm.tar.gz"
-libreoffice_langpack_url="https://download.documentfoundation.org/libreoffice/stable/7.6.4/rpm/x86_64/LibreOffice_7.6.4_Linux_x86-64_rpm_langpack_es.tar.gz"
+libreoffice_langpack_url="https://download.documentfoundation.org/libreoffice/stable/7.6.4/rpm/x86_64/LibreOffice_7.6.4_Linux_x86-64_rpm_langpack_es.tar.gz" # This is Spanish langpack, replace accordingly to your needs
 bluetooth_firmware_url="https://github.com/FrezeeB/Fedora-script/raw/bc43c58dabc3dde74adb040134f805c257b6f048/BCM43142A0-0a5c-216d.hcd"
+# Insert other packages if you need
 
 # Start
 if [ -e "$flag_file" ]; then
@@ -23,6 +25,7 @@ if [ -e "$flag_file" ]; then
     #Install broadcom driver
     echo "Installing wifi driver..."
     sudo dnf install broadcom-wl -y
+    sudo dnf install broadcom-bt-firmware -y
 
     #Compiling kernel modules and updating boot image
     echo "Loading kernel modules..."
@@ -30,7 +33,7 @@ if [ -e "$flag_file" ]; then
     sudo dracut --force
 
     #Remove unused bluetooth firmware
-    echo "Deleting unused bluetooth firmware files..."
+    echo "Deleting unused broadcom bluetooth firmware files..."
     sudo rm -f /lib/firmware/brcm/*xz
 
     #Install bluetooth firmware
@@ -59,10 +62,10 @@ else
     sudo dnf remove -y gnome-connections
     sudo dnf remove -y gnome-boxes
     sudo dnf remove -y gnome-font-viewer
+    sudo dnf remove -y *libreoffice*
     sudo dnf remove -y *iwl* # Do not remove if you have an intel wireless card
     sudo dnf remove -y *nvidia* # Do not remove if you have nvidia gpu
     sudo dnf remove -y *amd*gpu # Do not remove if you have amd gpu
-    sudo dnf remove -y *libreoffice*
 
     # Make dnf faster
     echo "Tweaking dnf config..."
